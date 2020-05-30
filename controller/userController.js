a0d2afd248317adb553ffc45d8a387f0f164a260
crusiris <cruzsirys@gmail.com>
2020-05-30 16:39:22 -0400

    Validando que el correo del usuario sea unico

--- controller/userController.js
+++ controller/userController.js
@@ -1,16 +1,26 @@
 const User = require('../models/UserModel'); //importando el modelo del usuario
 
 exports.createUser = async(req, res) => {
+
+    //Destructuring del req
+    const { email, password } = req.body;
+
     try {
-        let user;
-        //Creando el nuevo usuario
+
+        let user = await User.findOne({ email })
+
+        //Validando que el usuario/correo no exista
+        if (user) {
+            return res.status(400).json({ msg: 'El usuario ya eiste' });
+        }
+
         user = new User(req.body); //Creando una nueva instancia de user
 
         //guardar usuario
         await user.save();
 
         //Mensaje de confirmacio
-        res.send('El usuario se a creado correctamente');
+        res.status(400).json({ msg: 'El usuario fue creado correctamente' });
 
     } catch (error) {
         console.log(error);
