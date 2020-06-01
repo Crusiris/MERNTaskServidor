c298ea414757a96001e7a307dbef5c24eb44468d
crusiris <cruzsirys@gmail.com>
2020-06-01 10:46:42 -0400

    Insertando los proyectos creados en la base de datos

--- controller/projectController.js
+++ controller/projectController.js
@@ -1,3 +1,14 @@
-exports.createProject = async(req, res) => {
+const Project = require('../models/ProjectModel'); //importando el modelo
 
+exports.createProject = async(req, res) => {
+    try {
+        //Creando proyecto
+        const project = new Project(req.body);
+        project.save(); //Guardando la nueva instancia proyecto en BD
+        res.json(project);
+    } catch (error) {
+        console.log(error); //Imprimiendo el error en consola
+        res.status(500).send('Hubo un error'); // Enviando un mensaje de error
+
+    }
 }
