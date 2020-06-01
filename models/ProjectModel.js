51ffa0eb7c7bdd0cc67a005197603eca43ceb327
crusiris <cruzsirys@gmail.com>
2020-06-01 09:25:00 -0400

    Creando modelo para proyectos

---
+++ models/ProjectModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose'); //Importando mongoose
+
+mongoose.set('useCreateIndex', true);
+//Funcion para definir el modelo o estructura de la base de datos
+
+const ProjectsSchema = mongoose.Schema({
+
+    name: {
+        type: String,
+        require: true,
+        trim: true //Eliminar espacios en blanco
+    },
+
+    usercreate: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+    datecreate: {
+        type: Date,
+        default: Date.now() //"now()" funcion que genera una fecha al instante
+    }
+
+});
+
+module.exports = mongoose.model('Project', ProjectsSchema); //('nombreModelo', UsersSchema);
